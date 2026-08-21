bf5d8f883b2b75c6da1b88daa41d8f0ce7a066c4
Tammishetti Gouthami <gouthamitammishetti116@gmail.com>
2026-08-21 12:19:44 +0530

    message1

---
+++ sum.java
@@ -0,0 +1,11 @@
+public class Sum
+  {
+    public static void main(String args[])
+    {
+      String s1=args[0];
+      String s2=args[1];
+      int a=Integer.parseInt(s1);
+      int b=Integer.parseInt(s2);
+      System.out.println("sum="+(a+b));
+    }
+  }
